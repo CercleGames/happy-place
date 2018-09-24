7ad8e5560cb8acb4245516d65a615ba700586b29
Cercle Games <43051950+CercleGames@users.noreply.github.com>
2018-09-24 14:17:38 -0700

    Rollercart Added and Lorikeet Born

---
+++ Happy Place Ltd VR/Assets/RollercoasterScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollercoasterScript : MonoBehaviour {
+
+
+    Animator anim;
+    public float speedMultipler = 2f;
+    public float ease = .01f;
+
+    float speed;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float yAxis = Input.GetAxis("Vertical");
+
+        speed = Mathf.Lerp(yAxis, speed, ease);
+
+        anim.SetFloat("Speed", speed);
+
+	}
+}
