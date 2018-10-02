6614f214ed73a440da56535a0c64869dfd00f256
Cercle Games <43051950+CercleGames@users.noreply.github.com>
2018-10-01 18:42:22 -0700

    bopo Interaction

---
+++ Happy Place Ltd VR/Assets/HandScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class HandScript : MonoBehaviour {
+
+
+    public VRTK_AvatarHandController handController;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+
+        if (handController.controllerEvents.triggerPressed && anim.GetBool("TriggerPressed") == false)
+        {
+            anim.SetBool("TriggerPressed", true);
+
+        } else if(handController.controllerEvents.triggerPressed == false && anim.GetBool("TriggerPressed") == true)
+        {
+            anim.SetBool("TriggerPressed", false);
+        }
+    }
+
+	
+}
